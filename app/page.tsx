3aa87d9565df35c27c8d46a07a57c10bd9e37d9a
Sátvik Karanam <89281036+skara9@users.noreply.github.com>
2024-08-24 01:57:23 +0000

    feat: main page with resizable panels

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,25 @@
-import styles from "./page.module.css";
+"use client";
+import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
+import Editor from "./ui/editor/editor";
+import Graph from "./ui/graph/graph";
+import Terminal from "./ui/terminal/terminal";
 
 export default function Page() {
-  return <main></main>;
+  return (
+    <main>
+      <PanelGroup autoSaveId="main" direction="vertical">
+        <Panel collapsible={true} defaultSize={30} order={1}>
+          <Graph />
+        </Panel>
+        <PanelResizeHandle />
+        <Panel collapsible={true} defaultSize={30} order={2}>
+          <Editor />
+        </Panel>
+        <PanelResizeHandle />
+        <Panel collapsible={true} defaultSize={40} order={3}>
+          <Terminal />
+        </Panel>
+      </PanelGroup>
+    </main>
+  );
 }
