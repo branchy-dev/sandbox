74d4d3e2d106b28909b151005986461610ff997a
Sátvik Karanam <89281036+skara9@users.noreply.github.com>
2024-08-24 04:49:06 +0000

    feat: add header and footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Footer from "./ui/footer/footer";
+import Header from "./ui/header/header";
 
 export const metadata: Metadata = {
   title: "Branchy",
@@ -13,7 +15,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
---
+++ app/ui/footer/footer.tsx
@@ -0,0 +1,11 @@
+import styles from "./footer.module.css";
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <div>&copy; 2024 Sátvik Karanam</div>
+      <hr />
+      <div>Branchy v0.1.0</div>
+    </footer>
+  );
+}
