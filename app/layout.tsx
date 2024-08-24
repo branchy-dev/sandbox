8d5a97a67801ac49522abac5b9e1d5ca98a98fc8
Sátvik Karanam <89281036+skara9@users.noreply.github.com>
2024-08-24 17:14:51 +0000

    feat: add favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,33 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="icon" type="image/png" href="/favicon.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#303030" />
+        <meta name="apple-mobile-web-app-title" content="Branchy" />
+        <meta name="application-name" content="Branchy" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+      </head>
       <body>
         <Header />
         {children}
--- app/ui/header/header.tsx
+++ app/ui/header/header.tsx
@@ -1,8 +1,12 @@
+import Logo from "../logo/logo";
 import styles from "./header.module.css";
 
 export default function Header() {
   return (
     <header className={styles.header}>
+      <div className={styles.logo}>
+        <Logo />
+      </div>
       <h1>Branchy</h1>
     </header>
   );
