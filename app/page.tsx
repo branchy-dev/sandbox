b437d275de07f66e8cd9ef2cb9f3ffcd763cd73a
Sátvik Karanam <89281036+skara9@users.noreply.github.com>
2024-08-24 15:41:58 +0000

    fix: main content sizing

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState } from "react";
 import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
+import styles from "./page.module.css";
 import Editor from "./ui/editor/editor";
 import Graph from "./ui/graph/graph";
 import Terminal from "./ui/terminal/terminal";
@@ -9,7 +10,7 @@ export default function Page() {
   const [update, setUpdate] = useState<number>(0);
 
   return (
-    <main>
+    <main className={styles.main}>
       <PanelGroup autoSaveId="main" direction="vertical">
         <Panel collapsible={true} defaultSize={30} order={1}>
           <Graph update={update} />
